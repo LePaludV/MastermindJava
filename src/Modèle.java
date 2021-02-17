f08a7f4632167f8c3316341f2735121a3b8629d4
Gabriel Cadilhac <gcadilha@pcalb-mm0000.univ-jfc.fr>
2021-02-17 10:42:33 +0100

    Variable du jeu

--- src/Modèle.java
+++ src/Modèle.java
@@ -1,6 +1,13 @@
+import java.awt.Color;
 
 public class Modèle {
-
+	
+	Color[] COULEURS = {Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.RED, Color.ORANGE, Color.BLACK, Color.WHITE};
+	
+	int N_TENTATIVES = 10;
+	int DIFFICULTE = 4;
+	//enum Etat = {EN_COURS, GAGNE, PERDU};
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
