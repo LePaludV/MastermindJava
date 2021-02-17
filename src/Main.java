f316df377f64752cb76f0280bcb8530530bdbd09
Valentin Le Palud <vlepalud@pcalb-mm0000.univ-jfc.fr>
2021-02-17 11:13:19 +0100

    ajout des boutons dans la vue et création du main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Modèle modl = new Modèle();
+		new Vue(modl);
+	
+
+	}
+
+}
--- src/Vue.java
+++ src/Vue.java
@@ -1,9 +1,94 @@
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.ArrayList;
 
-public class Vue {
+public class Vue extends Frame implements WindowListener{
+	Modèle mdl;
+	
+	Frame Jeu = new Frame();
+	Panel VueClavier =new Panel();
+	
+	public Vue (Modèle mdl){
+		this.mdl=mdl;
+			
+		
+		this.add(VueClavier);
+		
+		Color[] Couleur=this.mdl.COULEURS;
+		
+		for(int i=0;i<Couleur.length;i++) {
+			Button b = new Button();
+			b.setBackground(Couleur[i]);
+			this.VueClavier.add(b);
+		}
+		
+		
+		
+		this.setSize(500,500);
+		this.setTitle("Visualisation d'images");
+		this.pack();
+		this.setVisible(true);
+		this.addWindowListener(this);
+
+		}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	}
 
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+		
+	}
+
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
