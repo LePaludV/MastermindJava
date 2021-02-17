ec4b38a2beffc06c7f7bd2916768dd0e9baf9939
Valentin Le Palud <vlepalud@pcalb-mm0000.univ-jfc.fr>
2021-02-17 11:29:56 +0100

    ajout du Canvas dans la class Vue pour afficher le jeu

--- src/Vue.java
+++ src/Vue.java
@@ -1,7 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Panel;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -12,13 +16,17 @@ public class Vue extends Frame implements WindowListener{
 	
 	Frame Jeu = new Frame();
 	Panel VueClavier =new Panel();
+	Canvas VueProposition= new Canvas();
 	
 	public Vue (Modèle mdl){
 		this.mdl=mdl;
 			
 		
-		this.add(VueClavier);
 		
+		this.setLayout(new BorderLayout());
+		this.add(VueClavier,BorderLayout.SOUTH);
+		this.add(VueProposition,BorderLayout.NORTH);
+
 		Color[] Couleur=this.mdl.COULEURS;
 		
 		for(int i=0;i<Couleur.length;i++) {
@@ -27,16 +35,22 @@ public class Vue extends Frame implements WindowListener{
 			this.VueClavier.add(b);
 		}
 		
+		this.VueProposition.setPreferredSize(new Dimension(400,600));
 		
 		
-		this.setSize(500,500);
-		this.setTitle("Visualisation d'images");
+		this.setSize(500, 500);
+		this.setTitle("Mastermind");
 		this.pack();
 		this.setVisible(true);
 		this.addWindowListener(this);
 
 		}
 
+	public void paint(Graphics g) {
+		Graphics2D g2;
+        g2 = (Graphics2D) g;
+        g2.drawString ("It is a custom canvas area", 70, 70);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
